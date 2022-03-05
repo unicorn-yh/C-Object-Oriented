4fe4757f8e9c19ea5188c3aa79612b38037b1aae
unicornftk <laiyunghwei@gmail.com>
2022-03-05 21:16:35 +0800

    Create Hello.cpp

---
+++ homework1/Hello.cpp
@@ -0,0 +1,13 @@
+/*Modify Hello.cpp (The example program is on the page 103 of our uploaded textbook.) 
+so that it prints out your name and age (or shoe size, or your dog’s age, if that makes you feel better). 
+Compile and run the program. */
+
+#include <iostream>
+using namespace std;
+int main(){
+    cout << "My name is Olivia. I am " << 21 << " this year." << endl;
+    cout << "I love my family." << endl;
+    return 0;
+}
+
+
