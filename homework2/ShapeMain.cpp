c50cecf71f3f010075f6414f790c3bf704549ba8
unicornftk <laiyunghwei@gmail.com>
2022-03-07 21:11:48 +0800

    Create ShapeMain.cpp

---
+++ homework2/ShapeMain.cpp
@@ -0,0 +1,19 @@
+//ShapeMain.cpp
+#include <iostream>
+#include "ShapeLib.cpp"
+using namespace std;
+int main(){
+    Rectangle r;
+    char c='A',ch;
+    char *cAddress = &c;
+    float area=0.0;
+    cout << "Shape task assgined." << endl;
+    r.initialize("Rectangle");
+    r.setLength(3.5,5.5);
+    area=r.calculateArea();
+    cout << "Area: " << area << endl;
+    ch=r.setId(cAddress);
+    cout << "Shape index: " << ch << endl;
+    cout << "Task completed." << endl;
+    return 0;
+}
