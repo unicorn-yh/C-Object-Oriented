0411f765222039ee1a6a0525a2835e4bab244ad2
unicornftk <laiyunghwei@gmail.com>
2022-03-30 09:11:25 +0800

    Create CircleArea.cpp

---
+++ prac1/CircleArea.cpp
@@ -0,0 +1,11 @@
+//Print area of the circle by asking radius
+#include <iostream>
+using namespace std;
+
+int main(){
+    float radius;
+    cout << "Enter the radius of the circle: ";
+    cin >> radius;
+    cout << "The are of the circle: " << radius << endl;
+    return 0;
+}
