53346f5f1b9ebab07c08f78e1b8aa8663edaf630
unicornftk <laiyunghwei@gmail.com>
2022-04-02 00:55:12 +0800

    Create SizeOfOperator.cpp

---
+++ prac1/SizeOfOperator.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+    cout<<"bool:"<<sizeof(bool)<<endl; //1
+    cout<<"char:"<<sizeof(char)<<endl; //1
+    cout<<"int:"<<sizeof(int)<<endl;   //4
+    cout<<"float:"<<sizeof(float)<<endl; //4
+    cout<<"double:"<<sizeof(double)<<endl; //8
+    cout<<"long double:"<<sizeof(long double)<<endl; //8
+}
