df34ac033b2a1afcc464c64c4092ef6d4fd6267b
unicornftk <laiyunghwei@gmail.com>
2022-04-27 12:30:13 +0800

    Update LinearList.h

--- experiment7/LinearList.h
+++ experiment7/LinearList.h
@@ -2,7 +2,7 @@
 //LinearList.h
 #ifndef LinearList_h
 #define LinearList_h
-template <typename T>
+template <class T>
 class LinearList{
     public:
         LinearList(){}
@@ -10,15 +10,14 @@ class LinearList{
         virtual int Size() const=0;    //表容量
         virtual int Length() const=0;  //表长
         virtual int Search(T& x) const=0;  //搜索x值
-        virtual int Locate(T& i) const=0;  //定位第i项
         virtual bool getData(int i,T& x) const=0; //获取第i项
         virtual void setData(int i,T& x) =0; //修改第i项
         virtual bool Insert(int i,T& x) =0; //第i项后插入x
-        virtual bool Remove(int i,T& x) =0; //删除第i项
+        virtual bool Remove(int i) =0; //删除第i项
         virtual bool IsEmpty() const=0; //判表空
         virtual bool IsFull() const=0;  //判表满
-        virtual void Sort() =0;   //排序
-        virtual void input() =0;  //输入
+        virtual void Sort(bool asc) =0;   //排序
+        virtual void input(T& x) =0;  //输入
         virtual void output() =0;  //输出
 };
 #endif
