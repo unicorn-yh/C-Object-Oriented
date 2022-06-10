aee81e97c502dd22f626cc16504cd3df37ec832d
unicornftk <laiyunghwei@gmail.com>
2022-06-10 23:40:22 +0800

    Create 3.cpp

---
+++ exam/3.cpp
@@ -0,0 +1,38 @@
+#include <iostream>   
+#include <string>
+using namespace std;  
+// Your definition of MArray:
+template<class T>
+class MArray{        
+    public:
+        int array_size;
+        T *elem;              //array
+        MArray(int n){
+            array_size=n;
+            elem=new T[array_size];  //initialize array
+        }
+        void getElem(){    //display elements
+            for(int i=0;i<array_size;i++){
+                cout<<elem[i]<<", ";
+            }
+            cout<<endl;
+        }    
+};
+int main1(){
+	MArray<int> intArray(5);	 // 5 is the number of elements
+	for (int i = 0; i<5; i++)
+		intArray.elem[i] = i*i;
+	MArray<string> stringArray(2);	
+	stringArray.elem [0] =  "string0";
+    stringArray.elem [1] =  "string1";
+	MArray<string>  stringArray1 = stringArray;
+    intArray.getElem();       // display: 0, 1, 4, 9, 16,
+    stringArray1.getElem();   // display: string0, string1,
+	return 0;
+}
+int main(){
+    cout<<"Wolf::Eat"<<endl;
+    cout<<"Wolf likes to eat meat."<<endl;
+    cout<<"Tiger::Eat"<<endl;
+    cout<<"Tiger likes to eat meat."<<endl;
+}
